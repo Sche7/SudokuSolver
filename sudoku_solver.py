05eeccaa0c27fa31ed4ed2ab68b0fbb533781a25
Steven Cheung <stevencheung7@outlook.dk>
2021-07-26 13:22:33 +0200

    implemented validation methods

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,15 +1,59 @@
-import numpy as np
+from typing import Union
+from nptyping import NDArray
 
 
 class SudokuSolver:
     def __init__(self):
         pass
 
+    def from_numpy_array(self, array):
+        self.board = array.astype(int)
+
+    def _filter_off_zeros(self, x: list):
+        """
+        Helper method to filter off zeros in list
+        """
+        return [i for i in x if i != 0]
+
+    def has_repeated_number(self, x: Union[list, NDArray]):
+        return len(set(x)) != len(x)
+
+    def validate_rows(self, board):
+        nrow, _ = board.shape
+        for i in range(nrow):
+            row = self._filter_off_zeros(board[i, :])
+            if self.has_repeated_number(row):
+                return False
+        return True
+
+    def validate_columns(self, board):
+        _, ncol = board.shape
+        for i in range(ncol):
+            col = self._filter_off_zeros(board[:, i])
+            if self.has_repeated_number(col):
+                return False
+        return True
+
+    def validate_subgrids(self, board):
+        # TODO: Improve this
+        indices = [0, 3, 6]
+        for i in indices:
+            for j in indices:
+                subgrid = board[i:(i+3), j:(j+3)]
+                filtered_subgrid = self._filter_off_zeros(subgrid.flatten())
+                if self.has_repeated_number(filtered_subgrid):
+                    return False
+        return True
+
     def is_valid(self, board):
-        pass
+        return (
+            self.validate_rows(board) and
+            self.validate_columns(board) and
+            self.validate_subgrids(board)
+        )
 
     def is_complete(self, board):
-        pass
+        return 0 not in board
 
     def run(self):
         pass
--- tests/test_validation_methods.py
+++ tests/test_validation_methods.py
@@ -9,7 +9,7 @@ def sudoku_solver():
     return solver
 
 
-def test_empty_board(sudoku_solver):
+def test_empty_board_is_valid(sudoku_solver):
     """
     Check that empty board is valid
     """
@@ -42,7 +42,7 @@ def test_complete_board_is_valid(sudoku_solver):
         [9, 6, 1, 5, 3, 7, 2, 8, 4],
         [2, 8, 7, 4, 1, 9, 6, 3, 5],
         [3, 4, 5, 2, 8, 6, 1, 7, 9]
-    ])
+    ], dtype=int)
 
     # See that board is indeed complete
     assert sudoku_solver.is_complete(complete_board)
@@ -53,11 +53,11 @@ def test_complete_board_is_valid(sudoku_solver):
 
 def test_complete_board_invalid_row(sudoku_solver):
     """
-    The complete board in this test is incorrectly solved.
-    See that complete board is invalid on first row.
+    The board in this test is incorrectly solved.
+    See that rge board is invalid on first row.
     """
-    complete_board = np.array([
-        [5, 3, 4, 6, 7, 8, 9, 1, 5],
+    board = np.array([
+        [5, 3, 4, 6, 7, 8, 9, 1, 9],
         [6, 7, 2, 1, 9, 5, 3, 4, 8],
         [1, 9, 8, 3, 4, 2, 5, 6, 7],
         [8, 5, 9, 7, 6, 1, 4, 2, 3],
@@ -65,38 +65,74 @@ def test_complete_board_invalid_row(sudoku_solver):
         [7, 1, 3, 9, 2, 4, 8, 5, 6],
         [9, 6, 1, 5, 3, 7, 2, 8, 4],
         [2, 8, 7, 4, 1, 9, 6, 3, 5],
-        [3, 4, 5, 2, 8, 6, 1, 7, 9]
-    ])
+        [3, 4, 5, 2, 8, 6, 1, 7, 0]
+    ], dtype=int)
 
-    # See that board is indeed complete
-    assert sudoku_solver.is_complete(complete_board)
+    # See that board is incomplete
+    assert sudoku_solver.is_complete(board) is False
 
-    # See that board is valid
-    assert sudoku_solver.is_valid(complete_board) is False
+    # See that board is valid column-wise
+    assert sudoku_solver.validate_columns(board)
+
+    # See that board is invalid row-wise
+    assert sudoku_solver.validate_rows(board) is False
 
 
 def test_complete_board_invalid_col(sudoku_solver):
     """
     The complete board in this test is incorrectly solved.
-    See that complete board is invalid on last column.
+    See that the board is invalid on last column.
     """
-    complete_board = np.array([
+    board = np.array([
         [5, 3, 4, 6, 7, 8, 9, 1, 2],
         [6, 7, 2, 1, 9, 5, 3, 4, 8],
         [1, 9, 8, 3, 4, 2, 5, 6, 7],
         [8, 5, 9, 7, 6, 1, 4, 2, 3],
         [4, 2, 6, 8, 5, 3, 7, 9, 1],
         [7, 1, 3, 9, 2, 4, 8, 5, 6],
-        [9, 6, 1, 5, 3, 7, 2, 8, 2],
+        [9, 6, 1, 5, 3, 7, 0, 8, 2],
         [2, 8, 7, 4, 1, 9, 6, 3, 5],
         [3, 4, 5, 2, 8, 6, 1, 7, 9]
-    ])
+    ], dtype=int)
 
-    # See that board is indeed complete
-    assert sudoku_solver.is_complete(complete_board)
+    # See that board is incomplete
+    assert sudoku_solver.is_complete(board) is False
 
-    # See that board is valid
-    assert sudoku_solver.is_valid(complete_board) is False
+    # See that board is valid row-wise
+    assert sudoku_solver.validate_rows(board)
+
+    # See that board is invalid column-wise
+    assert sudoku_solver.validate_columns(board) is False
+
+
+def test_invalid_subgrid(sudoku_solver):
+    """
+    The complete board in this test is incorrectly solved.
+    See that board is invalid in the subgrid.
+    """
+    board = np.array([
+        [5, 3, 4, 0, 0, 0, 0, 0, 0],
+        [6, 7, 2, 0, 0, 0, 0, 0, 0],
+        [1, 6, 8, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ], dtype=int)
+
+    # See that board is incomplete
+    assert sudoku_solver.is_complete(board) is False
+
+    # See that board is valid row-wise
+    assert sudoku_solver.validate_rows(board)
+
+    # See that board is valid column-wise
+    assert sudoku_solver.validate_columns(board)
+
+    # See that board is invalid subgrid-wise
+    assert sudoku_solver.validate_subgrids(board) is False
 
 
 def test_complete_board_is_invalid(sudoku_solver):
@@ -114,7 +150,7 @@ def test_complete_board_is_invalid(sudoku_solver):
         [9, 6, 1, 5, 3, 7, 2, 8, 4],
         [2, 8, 7, 4, 1, 9, 6, 3, 5],
         [3, 4, 5, 2, 8, 6, 1, 9, 9]  # two 9's are added
-    ])
+    ], dtype=int)
 
     # See that board is indeed complete
     assert sudoku_solver.is_complete(complete_board)
@@ -137,7 +173,7 @@ def test_incomplete_board_is_valid(sudoku_solver):
         [9, 6, 1, 5, 3, 7, 2, 8, 4],
         [2, 8, 7, 4, 1, 9, 6, 3, 5],
         [3, 4, 5, 2, 8, 6, 1, 7, 0]
-    ])
+    ], dtype=int)
 
     # See that board is indeed complete
     assert not sudoku_solver.is_complete(incomplete_board)
@@ -160,7 +196,7 @@ def test_incomplete_board_is_invalid(sudoku_solver):
         [9, 6, 1, 5, 3, 7, 2, 8, 4],
         [2, 8, 7, 4, 1, 9, 6, 7, 5],
         [3, 4, 5, 2, 8, 6, 1, 7, 0]
-    ])
+    ], dtype=int)
 
     # See that board is indeed complete
     assert sudoku_solver.is_complete(incomplete_board) is False
