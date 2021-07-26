431762f6709b3c95cd50f6fa5f8a217764951985
Steven Cheung <stevencheung7@outlook.dk>
2021-07-26 12:14:21 +0200

    initiated sudoku class

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(
+    name='Sudoku',
+    version='0.1',
+    author='Steven Cheung',
+    author_email='stevencheung7@outlook.dk',
+    description='A library that can solve any sudoku board'
+)
---
+++ sudoku_solver.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+class SudokuSolver:
+    def __init__(self):
+        pass
+
+    def is_valid(self, board):
+        pass
+
+    def is_complete(self, board):
+        pass
+
+    def run(self):
+        pass
