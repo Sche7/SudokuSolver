b668adbde24d1506bd210b314fbb0573260b44bc
Steven Cheung <stevencheung7@outlook.dk>
2022-05-15 10:23:42 +0200

    bump version

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ from setuptools import setup
 
 setup(
     name='Sudoku',
-    version='0.1',
+    version='1.0',
     author='Steven Cheung',
     author_email='stevencheung7@outlook.dk',
     description='A library that can solve any sudoku board',
-    py_modules=['run'],
+    py_modules=[
+      'run',
+      'sudoku_solver'
+    ],
     entry_points='''
         [console_scripts]
         solve=run:main
