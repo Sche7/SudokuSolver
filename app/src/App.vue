7fb6a3f196eadd217ce2532b49b4ba85a40d7e6c
Steven Cheung <stevencheung7@outlook.dk>
2023-01-07 17:31:19 +0100

    indent

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-    <link
+      <link
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/sketchy/bootstrap.min.css"
         integrity="sha384-RxqHG2ilm4r6aFRpGmBbGTjsqwfqHOKy1ArsMhHusnRO47jcGqpIQqlQK/kmGy9R"
