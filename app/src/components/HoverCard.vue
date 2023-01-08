616c104d63a193cd9fe59a06ea5e82715d03fa17
Steven Cheung <stevencheung7@outlook.dk>
2023-01-08 21:10:46 +0100

    initial card hover

---
+++ app/src/components/HoverCard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="card border-primary mb-3" style="max-width: 20rem;">
+        <div class="card-header">{{ header }}</div>
+        <div class="card-body">
+            <h4 class="card-title">{{ title }}</h4>
+            <p class="card-text">{{ cardText }}</p>
+    </div>
+</div>
+</template>
+
+<script>
+
+export default {
+    name: 'HoverCard',
+    props: ['header', 'title', 'cardText']
+}
+
+</script>
