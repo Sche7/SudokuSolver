70b91e8dd37cf334a01cb0c1ce6b72b64b13d099
Steven <stevencheung7@outlook.dk>
2025-01-25 19:05:23 +0100

    add forwardports

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -13,3 +13,15 @@ sudo apt install -y nodejs && sudo apt install -y npm;
 
 # Install Node.js dependencies
 make install-app;
+
+# Install Docker
+sudo apt install -y curl apt-transport-https ca-certificates software-properties-common;
+sudo apt install docker.io -y;
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg;
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null;
+sudo apt update -y;
+sudo apt install docker-ce -y;
+sudo usermod -aG docker $USER && newgrp;
+
+# Install Docker Compose
+sudo apt-get install -y docker-compose-plugin;
